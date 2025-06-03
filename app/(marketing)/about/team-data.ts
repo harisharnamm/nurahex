d3d15e875d3252d61837544a3857a2d64730b0fd
Harisharnam <168778202+harisharnamm@users.noreply.github.com>
2025-06-03 14:49:46 +0530

    Update team pictures

--- app/(marketing)/about/team-data.ts
+++ app/(marketing)/about/team-data.ts
@@ -6,7 +6,7 @@ export const TEAM: TeamMember[] = [
 		name: 'Eashan',
 		role: 'Co-Founder & Chief Executive Officer',
 		bio: 'AI visionary turning bold ideas into real products. Tech expert with strategic mindset. Based in Berlin.',
-		avatar: '/team/lakshya-512x512.webp',
+		avatar: 'https://media.licdn.com/dms/image/v2/D5603AQG_xjuX32znRg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1678224955632?e=1754524800&v=beta&t=TmuUpEjwg8wIbZdq5Mp8oWqUlo3GOvGf2TtdgHWLqa4',
 		socials: [
 			{ type: 'li', url: 'https://www.linkedin.com/in/eashan-punia-01b0bb142/' },
 		]
@@ -16,7 +16,7 @@ export const TEAM: TeamMember[] = [
 		name: 'Lakshya',
 		role: 'Co-Founder & Chief Operating Officer',
 		bio: 'Operations lead connecting tech and business. Full-stack dev who makes things happen. Based in Dubai.',
-		avatar: '/team/eashan-512x512.webp',
+		avatar: 'https://media.licdn.com/dms/image/v2/D4E03AQFRVYYk5ODDEw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1718798511997?e=1754524800&v=beta&t=6WWtD90LB3b8lNFxPFTXb0YiP7obr28Xzl7QPv3uVU8',
 		socials: [
 			{ type: 'li', url: 'https://www.linkedin.com/in/lakshya-joshi-3641971bb/' },
 		]
@@ -28,6 +28,6 @@ export const TEAM: TeamMember[] = [
 		bio: 'System architect who makes complex tech look easy. Builds the reliable engines powering our AI magic. Based in New Delhi.',
 		avatar: '/team/hari-512x512.webp',
 		socials: [
-			{ type: 'li', url: 'https://linkedin.com/in/harisharnam' },		]
+			{ type: 'li', url: 'https://media.licdn.com/dms/image/v2/D4D03AQG2BNkN-zfQdQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1700883835869?e=1754524800&v=beta&t=pPkMWOb7tMgXlx1puEBU7laFR5237zUy_JBXBpOSPq0' },		]
 	}
 ];
