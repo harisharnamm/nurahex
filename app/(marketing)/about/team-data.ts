08e49fdcacc434810b9da23707c56272847f3c4f
Harisharnam <168778202+harisharnamm@users.noreply.github.com>
2025-06-03 15:08:00 +0530

    Bio update

--- app/(marketing)/about/team-data.ts
+++ app/(marketing)/about/team-data.ts
@@ -5,7 +5,7 @@ export const TEAM: TeamMember[] = [
 		id: 1,
 		name: 'Eashan',
 		role: 'Co-Founder & Chief Executive Officer',
-		bio: 'AI visionary turning bold ideas into real products. Tech expert with strategic mindset. Based in Berlin.',
+		bio: 'AI visionary turning bold ideas into real products. Tech expert with strategic mindset. Based in Dubai.',
 		avatar: 'https://media.licdn.com/dms/image/v2/D5603AQG_xjuX32znRg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1678224955632?e=1754524800&v=beta&t=TmuUpEjwg8wIbZdq5Mp8oWqUlo3GOvGf2TtdgHWLqa4',
 		socials: [
 			{ type: 'li', url: 'https://www.linkedin.com/in/eashan-punia-01b0bb142/' },
@@ -15,7 +15,7 @@ export const TEAM: TeamMember[] = [
 		id: 2,
 		name: 'Lakshya',
 		role: 'Co-Founder & Chief Operating Officer',
-		bio: 'Operations lead connecting tech and business. Full-stack dev who makes things happen. Based in Dubai.',
+		bio: 'Operations lead connecting tech and business. Make business happen.  Based in Berlin.',
 		avatar: 'https://media.licdn.com/dms/image/v2/D4E03AQFRVYYk5ODDEw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1718798511997?e=1754524800&v=beta&t=6WWtD90LB3b8lNFxPFTXb0YiP7obr28Xzl7QPv3uVU8',
 		socials: [
 			{ type: 'li', url: 'https://www.linkedin.com/in/lakshya-joshi-3641971bb/' },
